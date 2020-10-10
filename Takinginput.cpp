328dac9ec73e12d6f976ea051b0fabf719590013
gitavinashksingh <72639231+gitavinashksingh@users.noreply.github.com>
2020-10-10 09:00:02 +0530

    Updated the code

--- Takinginput.cpp
+++ Takinginput.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
-int main()
+int main()//Main file
 {
 int n;
 cin>>n>>endl;
-cout<<"The value entered is "<<n;
+cout<<"The value entered is "<<n;//Printing the value entered
 }
